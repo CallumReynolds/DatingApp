c2ae276d24e2e7fa9195530f82287296c7d30322
Callum Reynolds <callum_rey@hotmail.com>
2019-11-11 15:07:25 +1000

    initial commit

---
+++ DatingApp-SPA/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+// responsible for providing data
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ok boomer';
+}
